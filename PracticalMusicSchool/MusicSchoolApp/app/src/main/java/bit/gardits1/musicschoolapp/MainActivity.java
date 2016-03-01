e7101d8adcb2c156918d81c82f94fa5bd00e9267
tsgardiner <tsgardiner@windowslive.com>
2016-03-01 22:51:02 +1300

    Revert "Completed"
    This reverts commit a7ea7b1058ea1cd03d3f75f540d2ab8df79ba389.

--- PracticalMusicSchool/MusicSchoolApp/app/src/main/java/bit/gardits1/musicschoolapp/MainActivity.java
+++ PracticalMusicSchool/MusicSchoolApp/app/src/main/java/bit/gardits1/musicschoolapp/MainActivity.java
@@ -2,53 +2,19 @@ package bit.gardits1.musicschoolapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.RadioButton;
 import android.widget.RadioGroup;
-import android.widget.Spinner;
-import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
-    private Spinner monthSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button confirmEnrolment = (Button) findViewById(R.id.confirmButton);
-        monthSpinner = (Spinner) findViewById(R.id.spinnerMonths);
-        String[] month = getResources().getStringArray(R.array.month);
-        int layoutId = android.R.layout.simple_spinner_dropdown_item;
-
-        ArrayAdapter<String> monthsAdapter = new ArrayAdapter<>(this, layoutId, month);
-        monthSpinner.setAdapter(monthsAdapter);
-
-        confirmEnrolment.setOnClickListener(new confirmEnrolmentButton());
+        RadioGroup instrumentGroup = (RadioGroup) findViewById(R.id.RadioGroup_Instruments);
+        instrumentGroup.setOnClickListener(new RadioGroupListener());
 
     }
 
-    public class confirmEnrolmentButton implements Button.OnClickListener
-    {
-        @Override
-        public void onClick(View v) {
-
-            RadioGroup instrumentGroup = (RadioGroup) findViewById(R.id.RadioGroup_Instruments);
-            int selectedId = instrumentGroup.getCheckedRadioButtonId();
-            RadioButton selectedInstrument = (RadioButton) findViewById(selectedId);
-
-            if(selectedId == -1)
-                Toast.makeText(MainActivity.this, "No instrument selected.", Toast.LENGTH_SHORT).show();
-            else if(monthSpinner.getSelectedItem().equals("Select Month"))
-                Toast.makeText(MainActivity.this, "Please choose a month.", Toast.LENGTH_SHORT).show();
-            else
-                Toast.makeText(MainActivity.this, "You have enrolled for " + selectedInstrument.getText() + " lessons on " + monthSpinner.getSelectedItem().toString(), Toast.LENGTH_SHORT).show();
-        }
-    }
-
-
+    public class RadioGroupListener implements RadioGroup
 }
