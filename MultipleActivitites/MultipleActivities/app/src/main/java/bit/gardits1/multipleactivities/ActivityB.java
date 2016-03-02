0923afc04c083be003e77314a714f51d593c61fc
tsgardiner <tsgardiner@windowslive.com>
2016-03-02 17:37:24 +1300

    Finished
    All activities working with web page loading

--- MultipleActivitites/MultipleActivities/app/src/main/java/bit/gardits1/multipleactivities/ActivityB.java
+++ MultipleActivitites/MultipleActivities/app/src/main/java/bit/gardits1/multipleactivities/ActivityB.java
@@ -1,7 +1,10 @@
 package bit.gardits1.multipleactivities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ActivityB extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class ActivityB extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activity_b);
+
+        Button btnActivityB = (Button) findViewById(R.id.btnActivityB);
+        btnActivityB.setOnClickListener(new btnListenerScreenB());
+
     }
+
+    public class btnListenerScreenB implements View.OnClickListener
+    {
+        @Override
+        public void onClick(View v) {
+            Intent changeActivityIntent = new Intent(ActivityB.this, ActivityC.class);
+            startActivity(changeActivityIntent);
+        }
+    }
+
 }
