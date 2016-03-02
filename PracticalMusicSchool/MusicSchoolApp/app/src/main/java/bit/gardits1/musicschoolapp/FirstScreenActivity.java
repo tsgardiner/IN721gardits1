703f89943884321bca63167478039ab16d9a8d01
tsgardiner <tsgardiner@windowslive.com>
2016-03-02 19:26:47 +1300

    Attempt at delayed start screen Debugger doesn't get to start activity code

--- PracticalMusicSchool/MusicSchoolApp/app/src/main/java/bit/gardits1/musicschoolapp/FirstScreenActivity.java
+++ PracticalMusicSchool/MusicSchoolApp/app/src/main/java/bit/gardits1/musicschoolapp/FirstScreenActivity.java
@@ -1,10 +1,12 @@
 package bit.gardits1.musicschoolapp;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
 
 public class FirstScreenActivity extends AppCompatActivity {
 
@@ -13,12 +15,23 @@ public class FirstScreenActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_screen);
 
-        Button btnFirstScreen = (Button) findViewById(R.id.btnFirstScreen);
-        btnFirstScreen.setOnClickListener(new btnListenerFirstScreen());
+        /*Button btnFirstScreen = (Button) findViewById(R.id.btnFirstScreen);
+        btnFirstScreen.setOnClickListener(new btnListenerFirstScreen());*/
+
+        Handler handler = new Handler();
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent openCourseSelection = new Intent(FirstScreenActivity.this, MainActivity.class);
+                startActivity(openCourseSelection);
+            }
+        }, R.integer.startDelayInMS);
 
     }
 
-    public class btnListenerFirstScreen implements View.OnClickListener
+
+   /* public class btnListenerFirstScreen implements View.OnClickListener
     {
 
         @Override
@@ -26,5 +39,5 @@ public class FirstScreenActivity extends AppCompatActivity {
             Intent openCourseSelection = new Intent(FirstScreenActivity.this, MainActivity.class);
             startActivity(openCourseSelection);
         }
-    }
+    }*/
 }
