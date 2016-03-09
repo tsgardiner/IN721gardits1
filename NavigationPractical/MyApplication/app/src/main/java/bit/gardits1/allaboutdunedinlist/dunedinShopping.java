822f9f029a38d6725b430d0c3ac76d85140216cb
tsgardiner <tsgardiner@windowslive.com>
2016-03-09 14:47:31 +1300

    Created activities

---
+++ NavigationPractical/MyApplication/app/src/main/java/bit/gardits1/allaboutdunedinlist/dunedinShopping.java
@@ -0,0 +1,13 @@
+package bit.gardits1.allaboutdunedinlist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class dunedinShopping extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dunedin_shopping);
+    }
+}
