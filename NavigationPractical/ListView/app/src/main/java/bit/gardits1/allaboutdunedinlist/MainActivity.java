7ab70994547b8004d11efcf7aca7712a0b09e0b2
tsgardiner <tsgardiner@windowslive.com>
2016-03-10 12:28:03 +1300

    Set up method

--- NavigationPractical/ListView/app/src/main/java/bit/gardits1/allaboutdunedinlist/MainActivity.java
+++ NavigationPractical/ListView/app/src/main/java/bit/gardits1/allaboutdunedinlist/MainActivity.java
@@ -2,6 +2,10 @@ package bit.gardits1.allaboutdunedinlist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +19,19 @@ public class MainActivity extends AppCompatActivity {
 
     public void setUpDunedinList()
     {
+        String[] groups = getResources().getStringArray(R.array.navigationList);
+        ArrayAdapter<String> groupAdapter = new ArrayAdapter<> (this, android.R.layout.simple_list_item_2, groups);
+        ListView groupListView = (ListView) findViewById(R.id.lvDunedinMainNav);
+        groupListView.setAdapter(groupAdapter);
+    }
 
+
+    public class DunendinListClickHandler implements AdapterView.OnItemClickListener
+    {
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+        }
     }
 }
