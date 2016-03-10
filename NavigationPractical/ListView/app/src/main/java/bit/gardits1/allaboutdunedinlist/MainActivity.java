e520a2f89dd45355fd4642f02541bb9904573597
tsgardiner <tsgardiner@windowslive.com>
2016-03-10 14:18:14 +1300

    All screens working No back function yet

--- NavigationPractical/ListView/app/src/main/java/bit/gardits1/allaboutdunedinlist/MainActivity.java
+++ NavigationPractical/ListView/app/src/main/java/bit/gardits1/allaboutdunedinlist/MainActivity.java
@@ -1,5 +1,6 @@
 package bit.gardits1.allaboutdunedinlist;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,12 +16,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         setUpDunedinList();
+
+        ListView groupListView = (ListView) findViewById(R.id.lvDunedinMainNav);
+        groupListView.setOnItemClickListener(new DunendinListClickHandler());
     }
 
     public void setUpDunedinList()
     {
         String[] groups = getResources().getStringArray(R.array.navigationList);
-        ArrayAdapter<String> groupAdapter = new ArrayAdapter<> (this, android.R.layout.simple_list_item_2, groups);
+        ArrayAdapter<String> groupAdapter = new ArrayAdapter<> (this, android.R.layout.simple_list_item_1, groups);
         ListView groupListView = (ListView) findViewById(R.id.lvDunedinMainNav);
         groupListView.setAdapter(groupAdapter);
     }
@@ -28,10 +32,32 @@ public class MainActivity extends AppCompatActivity {
 
     public class DunendinListClickHandler implements AdapterView.OnItemClickListener
     {
-
         @Override
         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            String itemClicked = (String) parent.getItemAtPosition(position);
+            Intent nextIntent;
 
-        }
+
+            switch(itemClicked)
+            {
+                case "Services":
+                    nextIntent = new Intent(MainActivity.this, dunedinServices.class);
+                    break;
+                case "Fun Things To Do":
+                    nextIntent = new Intent(MainActivity.this, dunedinActivities.class);
+                    break;
+                case "Dining":
+                    nextIntent = new Intent(MainActivity.this, dunedinDining.class);
+                    break;
+                case "Shopping":
+                    nextIntent = new Intent(MainActivity.this, dunedinShopping.class);
+                    break;
+                default:
+                    nextIntent = null;
+            }
+
+            if (nextIntent != null)
+                startActivity(nextIntent);
+        }//End of ItemClick
     }
 }
