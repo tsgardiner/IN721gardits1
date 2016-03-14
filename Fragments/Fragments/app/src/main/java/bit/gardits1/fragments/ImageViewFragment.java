6dc5ef76f049370ba5596c5b929311405fafd0d7
tsgardiner <tsgardiner@windowslive.com>
2016-03-14 16:13:17 +1300

    Image View Fragment

--- Fragments/Fragments/app/src/main/java/bit/gardits1/fragments/ImageViewFragment.java
+++ Fragments/Fragments/app/src/main/java/bit/gardits1/fragments/ImageViewFragment.java
@@ -3,7 +3,7 @@ package bit.gardits1.fragments;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,6 +16,9 @@ public class ImageViewFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
+
+
         return inflater.inflate(R.layout.fragment_image_view, container, false);
     }
 
--- Fragments/Fragments/app/src/main/java/bit/gardits1/fragments/MainActivity.java
+++ Fragments/Fragments/app/src/main/java/bit/gardits1/fragments/MainActivity.java
@@ -1,7 +1,12 @@
 package bit.gardits1.fragments;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnImageFragment = (Button) findViewById(R.id.btnImage);
+        btnImageFragment.setOnClickListener(new ButtonClickHandler());
+    }
+
+
+
+    public class ButtonClickHandler implements View.OnClickListener
+    {
+
+        @Override
+        public void onClick(View v) {
+            Fragment dynamicFragment = new ImageViewFragment();
+            FragmentManager fragmentManager = getFragmentManager();
+
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_image_container, dynamicFragment);
+            fragmentTransaction.commit();
+
+        }
     }
 }
