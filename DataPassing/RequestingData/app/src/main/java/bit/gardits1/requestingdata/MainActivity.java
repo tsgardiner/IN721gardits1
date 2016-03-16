2417050353bfd5af5dad68a23289e4ecf08c4e8a
tsgardiner <tsgardiner@windowslive.com>
2016-03-16 22:36:20 +1300

    Colour Change Complete Depreciated getColor is used

--- DataPassing/RequestingData/app/src/main/java/bit/gardits1/requestingdata/MainActivity.java
+++ DataPassing/RequestingData/app/src/main/java/bit/gardits1/requestingdata/MainActivity.java
@@ -1,9 +1,13 @@
 package bit.gardits1.requestingdata;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,7 +27,21 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onClick(View v) {
+            Intent changeIntent = new Intent(MainActivity.this, ColourChanger.class);
 
+            startActivityForResult(changeIntent, 0); //Should normally make integers xml for result id
         }
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if ((requestCode == 0) && (resultCode == Activity.RESULT_OK)) {
+            String colorResult = data.getStringExtra("requestedResult");
+
+            TextView textToChange = (TextView) findViewById(R.id.tvBodyText);
+            textToChange.setTextColor(Color.parseColor(colorResult));
+        }
+    }
+
 }
