64c3009d25118c39702a87801dfaf023b85cfbfa
tsgardiner <tsgardiner@windowslive.com>
2016-03-20 23:03:11 +1300

    Dialog Fragment Progress

--- PracticalMusicSchool/MusicSchoolApp/app/src/main/java/bit/gardits1/musicschoolapp/ConfirmDialog.java
+++ PracticalMusicSchool/MusicSchoolApp/app/src/main/java/bit/gardits1/musicschoolapp/ConfirmDialog.java
@@ -1,7 +1,12 @@
 package bit.gardits1.musicschoolapp;
 
 
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.app.Fragment;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,19 +16,45 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ConfirmDialog extends Fragment {
+public class ConfirmDialog extends DialogFragment {
 
 
     public ConfirmDialog() {
         // Required empty public constructor
     }
-
+    
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_confirm_dialog, container, false);
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Builder builder = new AlertDialog.Builder(getActivity());
+
+        builder.setTitle("Enrollment");
+        builder.setPositiveButton("Yes", new YesButtonHandler());
+        builder.setNegativeButton("No", new NoButtonHandler());
+
+        Dialog customDialog = builder.create();
+
+        return customDialog;
+    }
+
+    public class YesButtonHandler implements DialogInterface.OnClickListener
+    {
+
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            MainActivity mainActivity = (MainActivity) getActivity();
+
+            mainActivity.giveMeMyData(true);
+        }
+    }
+
+    public class NoButtonHandler implements DialogInterface.OnClickListener {
+
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            MainActivity mainActivity = (MainActivity) getActivity();
+            mainActivity.giveMeMyData(false);
+        }
     }
 
 }
--- PracticalMusicSchool/MusicSchoolApp/app/src/main/java/bit/gardits1/musicschoolapp/MainActivity.java
+++ PracticalMusicSchool/MusicSchoolApp/app/src/main/java/bit/gardits1/musicschoolapp/MainActivity.java
@@ -1,26 +1,33 @@
 package bit.gardits1.musicschoolapp;
 
+import android.app.FragmentManager;
+import android.content.DialogInterface;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements IGiveData {
 
     private Spinner monthSpinner;
+    ConfirmDialog confirm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button confirmEnrolment = (Button) findViewById(R.id.confirmButton);
+       Button confirmEnrolment = (Button) findViewById(R.id.confirmButton);
+
+
         monthSpinner = (Spinner) findViewById(R.id.spinnerMonths);
         String[] month = getResources().getStringArray(R.array.month);
         int layoutId = android.R.layout.simple_spinner_dropdown_item;
@@ -28,9 +35,15 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter<String> monthsAdapter = new ArrayAdapter<>(this, layoutId, month);
         monthSpinner.setAdapter(monthsAdapter);
 
-        confirmEnrolment.setOnClickListener(new confirmEnrolmentButton());
+        confirmEnrolment.setOnClickListener(new CreateFragmentButtonHandler());
 
     }
+
+    @Override
+    public boolean giveMeMyData(Boolean confirmation) {
+        return false;
+    }
+
 
     public class confirmEnrolmentButton implements Button.OnClickListener
     {
@@ -49,4 +62,16 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "You have enrolled for " + selectedInstrument.getText() + " lessons on " + monthSpinner.getSelectedItem().toString(), Toast.LENGTH_SHORT).show();
         }
     }
+
+    public class CreateFragmentButtonHandler implements View.OnClickListener
+    {
+
+        @Override
+        public void onClick(View v) {
+            confirm = new ConfirmDialog();
+            FragmentManager fm = getFragmentManager();
+            confirm.show(fm, "confirm");
+        }
+    }
+
 }
