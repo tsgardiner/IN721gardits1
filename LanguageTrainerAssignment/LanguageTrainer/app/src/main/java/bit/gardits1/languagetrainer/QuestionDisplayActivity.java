0d5c80aac4ec23c185e37f954b348fa2b92825f1
tsgardiner <tsgardiner@windowslive.com>
2016-03-27 16:22:20 +1300

    Basic Spinner and Confirmation working

--- LanguageTrainerAssignment/LanguageTrainer/app/src/main/java/bit/gardits1/languagetrainer/QuestionDisplayActivity.java
+++ LanguageTrainerAssignment/LanguageTrainer/app/src/main/java/bit/gardits1/languagetrainer/QuestionDisplayActivity.java
@@ -1,10 +1,13 @@
 package bit.gardits1.languagetrainer;
 
+import android.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class QuestionDisplayActivity extends AppCompatActivity implements IConfirmResullt{
 
@@ -12,13 +15,14 @@ public class QuestionDisplayActivity extends AppCompatActivity implements IConfi
 //    Will basically just change the Image based on the question loaded by the QuestionManager
 
     private Spinner answerSpinner;
+    private ConfirmAnswer confirmAnswer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question_display);
 
-        Button answerButton = (Button) findViewById(R.id.btnConfrimAns);
+        Button answerQuestion = (Button) findViewById(R.id.btnConfrimAns);
 
         //Spinner setup
         answerSpinner = (Spinner) findViewById(R.id.ansSpinner);
@@ -27,12 +31,28 @@ public class QuestionDisplayActivity extends AppCompatActivity implements IConfi
         ArrayAdapter<String> choicesAdapter = new ArrayAdapter<>(this, layoutId, choices);
         answerSpinner.setAdapter(choicesAdapter);
 
+        answerQuestion.setOnClickListener(new OpenConfirmationHandler());
 
+    }
 
+    public class OpenConfirmationHandler implements View.OnClickListener
+    {
+
+        @Override
+        public void onClick(View v) {
+            if(answerSpinner.getSelectedItem().equals("Select an answer"))
+                Toast.makeText(QuestionDisplayActivity.this, "Please select an answer.", Toast.LENGTH_LONG).show();
+            else{
+                confirmAnswer = new ConfirmAnswer();
+                FragmentManager fm = getFragmentManager();
+                confirmAnswer.show(fm, "Answer");
+            }
+        }
     }
 
     @Override
     public void confirmationResult(Boolean confirmation) {
+        confirmAnswer.dismiss();
 
     }
 }
