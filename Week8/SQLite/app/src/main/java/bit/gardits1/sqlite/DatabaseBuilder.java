84eef27d199f9f475e9e9addf024382d79857a77
tsgardiner <tsgardiner@windowslive.com>
2016-04-08 00:58:49 +1200

    DB Builder Fixes

---
+++ Week8/SQLite/app/src/main/java/bit/gardits1/sqlite/DatabaseBuilder.java
@@ -0,0 +1,39 @@
+package bit.gardits1.sqlite;
+
+import android.database.sqlite.SQLiteDatabase;
+
+/**
+ * Created by Tim on 08/04/2016.
+ */
+public class DatabaseBuilder {
+
+    SQLiteDatabase cityCountyDB;
+
+    public DatabaseBuilder(SQLiteDatabase cityCountyDB) {
+        this.cityCountyDB = cityCountyDB;
+
+        CreateTable();
+        InsertRecords();
+    }
+
+    private void CreateTable()
+    {
+        String createQuery =    "CREATE TABLE IF NOT EXISTS tblCity(" +
+                                "cityId INTEGER PRIMARY KEY AUTOINCREMENT," +
+                                "cityName TEXT NOT NULL," +
+                                "countryName TEXT NOT NULL);";
+        cityCountyDB.execSQL(createQuery);
+    }
+
+    private void InsertRecords()
+    {
+        cityCountyDB.execSQL("INSERT INTO tblCity (cityName, countryName) VALUES('Amsterdamn', 'The Netherlands')");
+        cityCountyDB.execSQL("INSERT INTO tblCity (cityName, countryName) VALUES('Berlin', 'Germany')");
+        cityCountyDB.execSQL("INSERT INTO tblCity (cityName, countryName) VALUES('Munich', 'Germany')");
+        cityCountyDB.execSQL("INSERT INTO tblCity (cityName, countryName) VALUES('Paris', 'France')");
+        cityCountyDB.execSQL("INSERT INTO tblCity (cityName, countryName) VALUES('Nice', 'France')");
+        cityCountyDB.execSQL("INSERT INTO tblCity (cityName, countryName) VALUES('Brighton', 'England')");
+        cityCountyDB.execSQL("INSERT INTO tblCity (cityName, countryName) VALUES('London', 'England')");
+    }
+
+}
