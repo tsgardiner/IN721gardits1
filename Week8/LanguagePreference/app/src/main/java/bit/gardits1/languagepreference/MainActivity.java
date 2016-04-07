154ab3c8bcee41767ff0d4a90c358fea6d848088
tsgardiner <tsgardiner@windowslive.com>
2016-04-07 22:06:15 +1200

    Language Colour Preference Complete

--- Week8/LanguagePreference/app/src/main/java/bit/gardits1/languagepreference/MainActivity.java
+++ Week8/LanguagePreference/app/src/main/java/bit/gardits1/languagepreference/MainActivity.java
@@ -2,18 +2,21 @@ package bit.gardits1.languagepreference;
 
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     SharedPreferences preferences;
     Editor prefsEditor;
+    private Spinner languageSpinner;
+    private Spinner colourSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,30 +29,34 @@ public class MainActivity extends AppCompatActivity {
         Button bntSelect = (Button) findViewById(R.id.btnSetLanguage);
         bntSelect.setOnClickListener(new SetHandler());
 
-        String languagePreference = preferences.getString("language", null);
-        if (languagePreference != null) {
-            String languageGreeting = getGreeting(languagePreference);
-            TextView tvGreeting = (TextView) findViewById(R.id.tvChosenLanguage);
-            tvGreeting.setText(languageGreeting);
-        }
+        int layoutId = android.R.layout.simple_spinner_dropdown_item;
+        //Language Spinner
+        languageSpinner = (Spinner) findViewById(R.id.spinLaugage);
+        String[] languages = getResources().getStringArray(R.array.languages);
+        ArrayAdapter<String> languageAdapter = new ArrayAdapter<>(this, layoutId, languages);
+        languageSpinner.setAdapter(languageAdapter);
 
+        //Colour Spinner
+        colourSpinner = (Spinner) findViewById(R.id.spinColour);
+        String[] colours = getResources().getStringArray(R.array.colours);
+        ArrayAdapter<String> coloursAdapter = new ArrayAdapter<>(this, layoutId, colours);
+        colourSpinner.setAdapter(coloursAdapter);
 
+        CheckUpdateSettings();
     }
 
     public class SetHandler implements View.OnClickListener {
 
         @Override
         public void onClick(View v) {
-            RadioGroup rg = (RadioGroup) findViewById(R.id.radioGroup);
-            int checkedId = rg.getCheckedRadioButtonId();
-            RadioButton checkedRB = (RadioButton) findViewById(checkedId);
-            String selectedLanguage = checkedRB.getText().toString();
+            String selectedLanguage = languageSpinner.getSelectedItem().toString();
+            String selectedColour = colourSpinner.getSelectedItem().toString();
 
+            prefsEditor.putString("textColour", selectedColour);
             prefsEditor.putString("language", selectedLanguage);
             prefsEditor.commit();
 
-            String greeting = getGreeting(selectedLanguage);
-
+            CheckUpdateSettings();
         }
     }
 
@@ -70,6 +77,39 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
         return greeting;
+    }
+
+
+    private int getTextColour(String textColour) {
+        int colourValue = 0;
+
+        switch (textColour) {
+            case ("Red"):
+                colourValue = ContextCompat.getColor(this, R.color.colorRed);
+                break;
+            case ("Blue"):
+                colourValue = ContextCompat.getColor(this, R.color.colorBlue);
+                break;
+            case ("Black"):
+                colourValue = ContextCompat.getColor(this, R.color.colorBlack);
+                break;
+            default:
+                break;
+        }
+        return colourValue;
+    }
+
+    private void CheckUpdateSettings()
+    {
+        String languagePreference = preferences.getString("language", null);
+        String textColourPreference = preferences.getString("textColour", null);
+        if ((languagePreference != null) && (textColourPreference != null)) {
+            String languageGreeting = getGreeting(languagePreference);
+            int colourValue = getTextColour(textColourPreference);
+            TextView tvGreeting = (TextView) findViewById(R.id.tvChosenLanguage);
+            tvGreeting.setText(languageGreeting);
+            tvGreeting.setTextColor(colourValue);
+        }
     }
 
 }
