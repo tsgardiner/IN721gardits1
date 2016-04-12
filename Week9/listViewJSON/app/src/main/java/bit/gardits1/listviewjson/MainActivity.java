9f565fddceddac2f3173596aca5af2f59f9e0fc7
tsgardiner <tsgardiner@windowslive.com>
2016-04-12 23:28:48 +1200

    JSON List View Completed
    Bug with html tags appearing in some descriptions
    Three of the events have the same description.

--- Week9/listViewJSON/app/src/main/java/bit/gardits1/listviewjson/MainActivity.java
+++ Week9/listViewJSON/app/src/main/java/bit/gardits1/listviewjson/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.res.AssetManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -22,7 +24,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     ListView listDisplay;
-    ArrayList<String> jsonData;
+    ArrayList<String> eventTitles;
     String JSONInput;
 
 
@@ -30,13 +32,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        eventTitles = new ArrayList<>();
+
         LoadDunedinEventsData();
+        CreateEventTitleList();
+
         Button btnDisplay = (Button) findViewById(R.id.btnFillList);
         listDisplay = (ListView) findViewById(R.id.lvDisplay);
 
+
+        listDisplay.setOnItemClickListener(new DisplayDescription());
         btnDisplay.setOnClickListener(new DisplayHandler());
-
-
     }
 
     public class DisplayHandler implements View.OnClickListener
@@ -44,20 +51,27 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onClick(View v) {
-
-            try {
-                JSONObject eventsData = new JSONObject(JSONInput);
-                JSONObject events =  eventsData.getJSONObject("events");
-                JSONArray eventArray = events.getJSONArray("event");
-
-                Toast.makeText(MainActivity.this, eventArray.toString(), Toast.LENGTH_LONG).show();
-
-
-            } catch (JSONException e) {
-                e.printStackTrace();
+            int id = android.R.layout.simple_list_item_1;
+            ArrayAdapter<String> titlesAdapter = new ArrayAdapter<>(MainActivity.this, id, eventTitles);
+            listDisplay.setAdapter(titlesAdapter);
             }
+    }
 
-            }
+
+    public class DisplayDescription implements AdapterView.OnItemClickListener
+    {
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            int itemClicked = position;
+
+            String description = GetEventDescription(itemClicked);
+
+            if (description != null)
+                Toast.makeText(MainActivity.this, description, Toast.LENGTH_LONG).show();
+            else
+                Toast.makeText(MainActivity.this, "No description", Toast.LENGTH_SHORT).show();
+        }
     }
 
     public void LoadDunedinEventsData()
@@ -82,5 +96,53 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void ToastAllJsonData()
+    {
+        try {
+            JSONObject eventsData = new JSONObject(JSONInput);
+            JSONObject events =  eventsData.getJSONObject("events");
+            JSONArray eventArray = events.getJSONArray("event");
 
+            Toast.makeText(MainActivity.this, eventArray.toString(), Toast.LENGTH_LONG).show();
+
+            } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void CreateEventTitleList()
+    {
+        try {
+            JSONObject eventsData = new JSONObject(JSONInput);
+            JSONObject events =  eventsData.getJSONObject("events");
+            JSONArray eventArray = events.getJSONArray("event");
+
+            for (int i = 0; i < eventArray.length(); i++) {
+                JSONObject event = eventArray.getJSONObject(i);
+                String eventTitle = event.getString("title");
+                eventTitles.add(eventTitle);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String GetEventDescription(int listItem)
+    {
+        String eventDescription = null;
+
+        try {
+            JSONObject eventsData = new JSONObject(JSONInput);
+            JSONObject events =  eventsData.getJSONObject("events");
+            JSONArray eventArray = events.getJSONArray("event");
+            JSONObject event = eventArray.getJSONObject(listItem);
+            eventDescription = event.getString("description");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return eventDescription;
+    }
 }
