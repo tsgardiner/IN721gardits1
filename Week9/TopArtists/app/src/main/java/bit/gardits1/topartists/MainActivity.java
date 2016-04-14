d226faf2d18e9513f191184880712f7be5a70796
tsgardiner <tsgardiner@windowslive.com>
2016-04-14 14:21:21 +1200

    Top Artists Basic List View Complete

--- Week9/TopArtists/app/src/main/java/bit/gardits1/topartists/MainActivity.java
+++ Week9/TopArtists/app/src/main/java/bit/gardits1/topartists/MainActivity.java
@@ -1,11 +1,18 @@
 package bit.gardits1.topartists;
 
+import android.content.res.AssetManager;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -14,20 +21,26 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     ListView listView;
+    ArrayList<String> artistsList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        artistsList = new ArrayList<>();
+
         Button btnMakeList = (Button) findViewById(R.id.btnShowList);
         btnMakeList.setOnClickListener(new ShowArtistsList());
 
         listView = (ListView) findViewById(R.id.lvArtists);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, artistsList);
+        listView.setAdapter(arrayAdapter);
 
 
 
@@ -38,22 +51,49 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onClick(View v) {
-
+            AsyncApiJson asyncApiJson = new AsyncApiJson();
+            asyncApiJson.execute();
         }
     }
 
-    public class AsynApiJson extends AsyncTask<Void, Void, String>
+    public void CreateArtistListenerList(String fetchedString)
+    {
+        try {
+            JSONObject topArtists = new JSONObject(fetchedString);
+            JSONObject artists =  topArtists.getJSONObject("artists");
+            JSONArray artistArray = artists.getJSONArray("artist");
+
+            for (int i = 0; i < artistArray.length(); i++) {
+                JSONObject currentArtist = artistArray.getJSONObject(i);
+                String name = currentArtist.getString("name");
+                String playcount = currentArtist.getString("playcount");
+
+                artistsList.add(name + " " + playcount);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void CreateListViewOfArtists() {
+        listView = (ListView) findViewById(R.id.lvArtists);
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, artistsList);
+        listView.setAdapter(arrayAdapter);
+    }
+
+    public class AsyncApiJson extends AsyncTask<Void, Void, String>
     {
 
         @Override
         protected String doInBackground(Void... params) {
             String jsonString = null;
 
-            String url  =   "http://ws.audioscrobbler.com/2.0/;" +
-                    "?method=artist.getTopTracks&artist=;" +
-                    "Sia;" +
-                    "&api_key=58384a2141a4b9737eacb9d0989b8a8c&limit=10;" +
-                    "&format=json";
+            String url  =   "http://ws.audioscrobbler.com/2.0/" +
+                            "?method=" +
+                            "chart.getTopArtists" +
+                            "&api_key=58384a2141a4b9737eacb9d0989b8a8c" +
+                            "&limit=10" +
+                            "&format=json";
 
             try {
                 URL urlObject = new URL(url);
@@ -85,9 +125,15 @@ public class MainActivity extends AppCompatActivity {
 
         protected void onPostExecute(String fetchedString)
         {
-
+            CreateArtistListenerList(fetchedString);
+            CreateListViewOfArtists();
+            
+            //TextView textView = (TextView) findViewById(R.id.tvTesting);
+            //textView.setText(fetchedString);
         }
     }
+
+
 
 
 }
